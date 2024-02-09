549ec608d7d2dec38f62d7ef49f1969bc945da12
Tushar <56352683+Mandloi2309@users.noreply.github.com>
2024-02-09 22:22:23 +0000

    Addition of plots

--- src/backend/bar_chart.py
+++ src/backend/bar_chart.py
@@ -1,17 +1,28 @@
 from pandas import DataFrame, read_csv, concat
 from src.resources import total_models, labels, reactive_categories
 from plotly.graph_objects import Figure, Bar
+from plotly_express import bar
 
 def get_bar_chart():
     models = DataFrame()
     for f in total_models.keys():
         file = total_models[f]
-        model = read_csv(file, usecols=['model_id', 'provider']).drop_duplicates()
+        model = read_csv(file, usecols=['model_id', 'provider', 'model_type']).drop_duplicates()
+        model['model_type'] = ['Organoid' if str(t).lower().__contains__('organoid') else t for t in model['model_type']]
+        model['model_type'] = [
+            'Cell Line' if str(t).lower().__contains__('cell') or str(t).lower().__contains__('pdc') or str(
+                t).lower().__contains__('2d') or str(t).lower().__contains__('2-d') else t for t in model['model_type']]
+        model['model_type'] = [
+            'Other' if str(t).lower().__contains__('other') or str(t).lower().__contains__('mixed') else t for t in
+            model['model_type']]
+        model = model.groupby('model_type').count()['model_id']
+        model = DataFrame(zip(model.index.tolist(), model.values.tolist()), columns=['Model Type', 'Model Count'])
         model['Release'] = labels[f]
-        models = concat([models, model])
-    models = models.groupby('Release').count().to_dict()['model_id']
-    models = DataFrame(list(models.items()), columns=['Release', 'Models'])
-    fig = Figure(data=[Bar(x=models['Release'], y=models['Models'], marker_color='#013e48')])
+        models = concat([models, model]).reset_index(drop=True)
+    models = models.iloc[::-1]
+    total = models.groupby('Release').sum()['Model Count'].to_dict()
+    models['Total Models'] = [total[r] for r in models['Release']]
+    fig = bar(models, x='Release', y='Model Count', color='Model Type', hover_data=['Total Models'])
     fig.update_layout(
         title=dict(text="Model Distribution per Data Release", yref='container')
     )
