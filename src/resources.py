73103544e8aa0c79c1d76fef4d8a1c142378fb92
Tushar <56352683+Mandloi2309@users.noreply.github.com>
2024-06-25 13:52:55 +0100

    Update

--- src/resources.py
+++ src/resources.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pandas import read_csv
 
-labels = {'DR_6.2': 'Data release 6.2', 'DR_6.1': 'Data release 6.1', 'DR_6.0': 'Data release 6.0',
+labels = {'DR_6.3': 'Data release 6.3', 'DR_6.2': 'Data release 6.2', 'DR_6.1': 'Data release 6.1', 'DR_6.0': 'Data release 6.0',
     #'latest': 'Latest Release',
           'DR_5.3': 'Data release 5.3', 'DR_5.2': 'Data release 5.2', 'DR_5.1': 'Data release 5.1',
           'DR_4.0': 'Data release 4.0',
